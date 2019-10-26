40aa3b7e072555cd8ca5143a7bb82180253109ff
smanda25 <54826896+smanda25@users.noreply.github.com>
2019-10-26 11:20:40 -0500

    Update project_2_fault_cvg.py
    Just fixed an indentation problem

--- project_2_fault_cvg.py
+++ project_2_fault_cvg.py
@@ -404,73 +404,75 @@ def Number_of_input_bits(bench_file):
 
 
 def tv_generation(bench_file, integer_seed ):
+
 # generating TV_A.txt
-		OutputFile = open('TV_A.txt', 'w')
+	OutputFile = open('TV_A.txt', 'w')
 #Calculate the number of input bits in bench file
-		number_of_input_bits = Number_of_input_bits(bench_file)
+	number_of_input_bits = Number_of_input_bits(bench_file)
 # use seed as starting point and extend zeros until length
 
-		temp = integer_seed
-#Generate 255 test vectors    
-		for i in range(255):
-				binary_value = decimalToBinary(temp)
-				rem = number_of_input_bits-len(binary_value)
-				binary_value = '0'*rem + binary_value
+	temp = integer_seed
+#Generate 255 test vectors
+	for i in range(255):
+			binary_value = decimalToBinary(temp)
+			rem = number_of_input_bits-len(binary_value)
+			binary_value = '0'*rem + binary_value
 #Writing in Output file to generate TV_A.txt
-				OutputFile.write(binary_value + '\n')
+			OutputFile.write(binary_value + '\n')
 # Incrementing Counter
-				temp += 1
+			temp += 1
 # generating TV_B.txt
 #resetting back to seed 
-		temp = integer_seed
-		OutputFile=open('TV_B.txt', 'w')
-		for i in range(255):
-				if(temp == 256):
-					temp = 0
-				else:
-					temp = temp
-				binary_value = decimalToBinary(temp)
+	temp = integer_seed
+	OutputFile=open('TV_B.txt', 'w')
+	for i in range(255):
+			if(temp == 256):
+				temp = 0
+			else:
+				temp = temp
+			binary_value = decimalToBinary(temp)
 # Making each seed 8 bits
-				rem = 8-len(binary_value)
-				binary_value = '0'*rem + binary_value
+			rem = 8-len(binary_value)
+			binary_value = '0'*rem + binary_value
 #determining and looping the number of times it needs to be appended
-				for j in range(math.ceil(number_of_input_bits/8)):
-					binary_value = binary_value + binary_value
-				OutputFile.write(binary_value[0:number_of_input_bits] + '\n')
-				temp += 1
+			for j in range(math.ceil(number_of_input_bits/8)):
+				binary_value = binary_value + binary_value
+			OutputFile.write(binary_value[0:number_of_input_bits] + '\n')
+			temp += 1
 # generating TV_C.txt
-		temp = integer_seed
-		OutputFile = open('TV_C.txt', 'w')
-		for i in range(255):
-				if(temp == 256):
-					temp = 0
-				else:
-					temp = temp
+	temp = integer_seed
+	OutputFile = open('TV_C.txt', 'w')
+	for i in range(255):
+			if(temp == 256):
+				temp = 0
+			else:
+				temp = temp
 #storing temp in another variabel for the sake of looping
-				temp1 = temp
-				bits_per_line = 0
+			temp1 = temp
+			bits_per_line = 0
 #run the loop till we get binary_value == number_of_input_bits 
-				for j in range(math.ceil(number_of_input_bits/8)):
+			for j in range(math.ceil(number_of_input_bits/8)):
 #Converting decimal temp1 to binary
-						binary_value = decimalToBinary(temp1)
-						if(len(binary_value) > 8):
-							binary_value = '00000000'
-							temp1 = 0
-						else:
-							binary_value = binary_value
+					binary_value = decimalToBinary(temp1)
+					if(len(binary_value) > 8):
+						binary_value = '00000000'
+						temp1 = 0
+					else:
+						binary_value = binary_value
 #find out number of zeros to append to binary value
-						rem = 8-len(binary_value)
+					rem = 8-len(binary_value)
 #append zeros to value
-						binary_value = '0'*rem + binary_value
-						if (bits_per_line + 8 <= number_of_input_bits):
-								OutputFile.write(binary_value)
-						else:
-								OutputFile.write(binary_value[0:(number_of_input_bits - bits_per_line )])
-								OutputFile.write('\n')
-						bits_per_line += 8
-						temp1 += 1
-				temp += 1
+					binary_value = '0'*rem + binary_value
+					if (bits_per_line + 8 <= number_of_input_bits):
+							OutputFile.write(binary_value)
+					else:
+							OutputFile.write(binary_value[0:(number_of_input_bits - bits_per_line )])
+							OutputFile.write('\n')
+					bits_per_line += 8
+					temp1 += 1
+			temp += 1
 # generating TV_D.txt
+
 # generating TV_E.txt
 
 
